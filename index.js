e319997abc2e54eaded606d4c8d78f809eb2cdc0
pritu HQ <contact.pritulx@gmail.com>
2021-07-19 12:04:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ function update() {
         <th scope="row">${index + 1}</th>
         <td>${element[0]}</td>
         <td>${element[1]}</td>
-        <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})">Done</button></td>
+        <td><button class="btn btn-sm btn-primary" onclick="deleted(${index})"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi-bi-check2-all" viewBox="0 0 16 16">
+        <path d="M12.354 4.354a.5.5 0 0 0-.708-.708L5 10.293 1.854 7.146a.5.5 0 1 0-.708.708l3.5 3.5a.5.5 0 0 0 .708 0l7-7zm-4.208 7-.896-.897.707-.707.543.543 6.646-6.647a.5.5 0 0 1 .708.708l-7 7a.5.5 0 0 1-.708 0z"/>
+        <path d="m5.354 7.146.896.897-.707.707-.897-.896a.5.5 0 1 1 .708-.708z"/>
+      </svg></button></td>
         </tr>`;
   });
   tableBody.innerHTML = str;
